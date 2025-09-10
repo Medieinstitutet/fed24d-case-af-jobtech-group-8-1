f3edd6c3f179d2c1894988d7799e159aa104309d
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-09-10 17:21:32 +0200

    add interface for the API-response

---
+++ src/models/IDescription.ts
@@ -0,0 +1,8 @@
+export interface IDescription {
+    // TODO: Kolla om det är text eller text_formatted vi ska använda
+    text?: string;
+    company_information?: string;
+    needs?: string;
+    requirements?: string;
+    conditions?: string;
+}
---
+++ src/models/IWorkplace_address.ts
@@ -0,0 +1,5 @@
+export interface IWorkplace_address {
+    municipality?: string;
+    country?: string;
+    street_address?: string;
+}
