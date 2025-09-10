a4956ec8ac80924c4c8a9761ef56e22c814cd532
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-09-10 13:39:22 +0200

    build: add af-designsystem CSS imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { DigiButton } from "@digi/arbetsformedlingen-react";
 import "./App.css";
+import { ButtonVariation } from "@digi/arbetsformedlingen";
 
 function App() {
-	return <></>;
+	return <>
+		<DigiButton afVariation={ButtonVariation.PRIMARY}>Detta är en knapp</DigiButton>
+	</>;
 }
 
 export default App;
