d849bfcd46cc2087b07877659c734ba8b3caae47
Alice Swahn <aliceswahn@hotmail.com>
2025-09-10 15:26:03 +0200

    feat(app): render AppFooter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { DigiButton } from "@digi/arbetsformedlingen-react";
 import "./App.css";
 import { ButtonVariation } from "@digi/arbetsformedlingen";
+import AppFooter from "./components//AppFooter";
 
 function App() {
 	return <>
 		<DigiButton afVariation={ButtonVariation.PRIMARY}>Detta är en knapp</DigiButton>
+		<AppFooter />
 	</>;
 }
 
