3dd788785f1352c9455203ecbe6339f77b79e730
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-09-11 14:31:38 +0200

    API: change sort to relevance

--- src/services/jobAdsService.ts
+++ src/services/jobAdsService.ts
@@ -4,7 +4,7 @@ import { get } from "./serviceBase";
 const BASE_URL = "https://jobsearch.api.jobtechdev.se/search?";
 
 export const getJobAds = async (searchTerm: string, page: number) => {
-	const url = `${BASE_URL}q=${searchTerm}%20-senior&offset=${page * 10 - 10}&limit=10&sort=pubdate-desc`;
+	const url = `${BASE_URL}q=${searchTerm}%20-senior&offset=${page * 10 - 10}&limit=10`;
 
 	const data = await get<IJobAdsResponse>(url);
 
