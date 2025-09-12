f18d10405a5ccd671c8ae256315295ae11557137
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-09-12 11:39:41 +0200

    add SavedJobs to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
+import { SavedJobs } from "./pages/SavedJobs";
 
 export const router = createBrowserRouter([
 	{
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
 				path: "/",
 				element: <Home />,
 			},
+			{
+				path: "/saved-jobs",
+				element: <SavedJobs />
+			}
 		],
 	},
 ]);
