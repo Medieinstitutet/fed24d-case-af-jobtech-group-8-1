8277bec9bbaf31ccf2a93e7f508a7a763f0f4fe9
Malin Rosenquist <malinrosenquist@outlook.com>
2025-09-15 13:57:35 +0200

    refactor: add and remove components

--- src/components/jobAd/JobAdDescription.tsx
+++ src/components/jobAd/JobAdDescription.tsx
@@ -1,5 +1,6 @@
-import { DigiTypography, DigiTypographyMeta } from "@digi/arbetsformedlingen-react";
+import { DigiLayoutContainer, DigiTypography, DigiTypographyMeta } from "@digi/arbetsformedlingen-react";
 import type { IJobAdDetailed } from "../../models/IJobAd";
+import { LayoutContainerVariation } from "@digi/arbetsformedlingen";
 
 type JobAdDescriptionProps = {
 	jobAd: IJobAdDetailed;
@@ -9,15 +10,28 @@ export const JobAdDescription = ({ jobAd }: JobAdDescriptionProps) => {
 	return (
 		<>
 			<DigiTypography>
-				{jobAd.description?.text_formatted ? (
-					<div dangerouslySetInnerHTML={{ __html: jobAd.description.text_formatted }}></div>
-				) : (
-					<p>Ingen beskrivning tillgänglig</p>
-				)}
-				<DigiTypographyMeta>
-					<p>Publicerad:</p>
-					<p slot="secondary">{new Date(jobAd.publication_date).toLocaleDateString("sv-SE")}</p>
-				</DigiTypographyMeta>
+				<DigiLayoutContainer
+					afVerticalPadding={false}
+					afVariation={LayoutContainerVariation.NONE}
+					afMarginBottom={true}
+				>
+					<h2>Om tjänsten</h2>
+					{jobAd.description?.text_formatted ? (
+						<div dangerouslySetInnerHTML={{ __html: jobAd.description.text_formatted }}></div>
+					) : (
+						<p>Ingen beskrivning tillgänglig</p>
+					)}
+				</DigiLayoutContainer>
+				<DigiLayoutContainer
+					afVerticalPadding={true}
+					afVariation={LayoutContainerVariation.NONE}
+					afMarginTop={true}
+				>
+					<DigiTypographyMeta afVariation="primary">
+						<p>Publicerad:</p>
+						<p slot="secondary">{new Date(jobAd.publication_date).toLocaleDateString("sv-SE")}</p>
+					</DigiTypographyMeta>
+				</DigiLayoutContainer>
 			</DigiTypography>
 		</>
 	);
