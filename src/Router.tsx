09103c6a14e9c3627326a254bd766f0efc3bd921
Malin Rosenquist <malinrosenquist@outlook.com>
2025-09-15 09:30:56 +0200

    feat: add JobAd.tsx to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
+import { JobAd } from "./pages/JobAd";
 
 export const router = createBrowserRouter([
 	{
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
 			{
 				path: "/",
 				element: <Home />,
+			},
+			{
+				path: "/jobad/:id",
+				element: <JobAd />,
 			},
 		],
 	},
