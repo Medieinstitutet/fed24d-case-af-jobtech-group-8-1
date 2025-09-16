27f255d4e2787fc0c74eee284d21675b67721d1d
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-09-16 15:22:24 +0200

    Style: adjust positioning of svg
    Co-Authored-By: Alice Swahn  <89841935+Aliceswahn@users.noreply.github.com>
    Co-Authored-By: Malin Rosenquist <183487075+MalinRosenquist@users.noreply.github.com>

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,6 +5,7 @@ import {
 	LayoutColumnsVariation,
 	TypographyHeadingJumboLevel,
 	TypographyHeadingJumboVariation,
+	TypographyVariation,
 } from "@digi/arbetsformedlingen";
 import {
 	DigiLayoutBlock,
@@ -12,7 +13,6 @@ import {
 	DigiMediaImage,
 	DigiTypography,
 	DigiTypographyHeadingJumbo,
-	DigiTypographyPreamble,
 } from "@digi/arbetsformedlingen-react";
 
 import hero_img from "../assets/hero_img.svg";
@@ -46,26 +46,30 @@ export const HeroSection = () => {
 			<div>
 				{mobile && (
 					<>
-						<DigiTypography>
-							<DigiTypographyPreamble>
+						<DigiTypography afVariation={TypographyVariation.LARGE}>
+							<p>
 								NextStep är en digital tjänst som hjälper studenter och nyutexaminerade att navigera i jobbdjungeln.
 								Genom att använda Arbetsförmedlingens öppna data samlar NextStep relevanta annonser för juniora roller,
 								praktikplatser och första-jobb inom olika branscher.
-							</DigiTypographyPreamble>
+							</p>
 						</DigiTypography>
-						<DigiMediaImage afSrc={hero_img} afHeight="500" afWidth="500" afAlt="Illustration av en man som håller en kikare"/>
+						<div style={{display: "flex", justifyContent: "center", alignItems: "flex-start", margin: "5rem 0"}}>
+							<DigiMediaImage afSrc={hero_img} afWidth="500" afAlt="Illustration av en man som håller en kikare"/>
+						</div>
 					</>
 				)}
 				{!mobile && (
 					<DigiLayoutColumns afElement={LayoutColumnsElement.DIV} afVariation={LayoutColumnsVariation.TWO}>
-						<DigiTypography>
-							<DigiTypographyPreamble>
+						<DigiTypography afVariation={TypographyVariation.LARGE}>
+							<p>
 								NextStep är en digital tjänst som hjälper studenter och nyutexaminerade att navigera i jobbdjungeln.
 								Genom att använda Arbetsförmedlingens öppna data samlar NextStep relevanta annonser för juniora roller,
 								praktikplatser och första-jobb inom olika branscher.
-							</DigiTypographyPreamble>
+							</p>
 						</DigiTypography>
-						<DigiMediaImage afSrc={hero_img} afHeight="500" afWidth="500" afAlt="Illustration av en man som håller en kikare"/>
+						<div style={{display: "flex", justifyContent: "center", alignItems: "flex-start"}}>
+							<DigiMediaImage afSrc={hero_img} afWidth="500" afAlt="Illustration av en man som håller en kikare"/>
+						</div>
 					</DigiLayoutColumns>
 				)}
 			</div>
